ee963b67e6c90a1eb791d19d109a824bf9bb3d23
Awn <awn@cryptolosophy.org>
2018-09-15 16:11:44 +0100

    Initial commit

---
+++ assets/js/particles/app.js
@@ -0,0 +1,9 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load(
+    'particles-js1', 'assets/js/particles/config.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
+particlesJS.load(
+    'particles-js2', 'assets/js/particles/config.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
